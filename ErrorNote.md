935d768433e8222049597dd18e3cb07c902a7ca2
hwuiinn <82766211+Huwiinn@users.noreply.github.com>
2023-09-24 17:49:11 +0900

    react-router-dom 사용이해

---
+++ ErrorNote.md
@@ -0,0 +1,15 @@
+문제 : can't not module react-router-dom
+
+해결 : @types(@types/react-router-dom)를 설치하고 React-router-dom을 설치하기만 하면 됩니다.
+yarn add --dev @types/react-router-dom 만 설치해주었을 때 문제의 에러가 발생. 기본 react-router-dom도 설치받아주었을 때 문제 해결되었습니다.
+
+Typescript react-router-dom은 최신 업데이트가 5ver임.
+--dev로 깔았을 때는 에러가 났는데, --dev를 지우고 일반 의존성에 넣어 설치했는데 정상 작동함.
+
+왜 그런지는 아직 잘 모르겠으나, 다시 이런일이 일어난다면 이 메모를 보고 확인해보자.
+
+<hr>
+
+참고 : https://stackoverflow.com/questions/68899565/what-causes-the-typescript-module-has-no-exported-member-ts2305-error-and-how
+
+https://stackoverflow.com/questions/56695271/react-router-and-typescript-throwing-cant-resolve-react-router-dom-error
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/posts">Post List</Link>
+        </li>
+        <li>
+          <Link to="/posts:id">Post Detail</Link>
+        </li>
+        <li>
+          <Link to="/posts/new">Post New</Link>
+        </li>
+        <li>
+          <Link to="/posts/edit/:id">Post Edit</Link>
+        </li>
+        <li>
+          <Link to="/posts/profile">My Profile</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<h1>Main Home Page</h1>} />
+        <Route path="/posts" element={<h1>Post Page</h1>} />
+        <Route path="/posts:id" element={<h1>Post Detail Page</h1>} />
+        <Route path="/posts/new" element={<h1>Post New Page</h1>} />
+        <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>} />
+        <Route path="/posts/profile" element={<h1>Profile Page</h1>} />
+        {/* 지정되어있지 않은 Route로 진입할 때, 강제로 Home으로 Navigate 시킴 */}
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
   );
 }
 
