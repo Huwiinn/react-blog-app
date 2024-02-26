720f4f100ec49975948ad378516768b34601b539
hwuiinn <gnldls8406@gmail.com>
2024-02-26 15:49:36 +0900

    Feat : firebase 메서드를 이용하여 로그아웃 기능 구현

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,19 +1,37 @@
 import { Link } from "react-router-dom";
 import PostList from "components/PostList";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { toast } from "react-toastify";
 
 const Profile = () => {
+  const auth = getAuth(app);
+
+  const onSignOut = async () => {
+    const auth = getAuth(app);
+
+    try {
+      await signOut(auth);
+      toast.success("로그아웃 되었습니다.", { position: "top-right" });
+    } catch (e) {
+      toast.error("로그아웃에 실패했습니다.", { position: "top-right" });
+    }
+  };
+
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__info">
           <div className="profile__image" />
           <div>
-            <div className="profile__email">test@test.com</div>
-            <div className="profile__name">유저이름</div>
+            <div className="profile__email">{auth?.currentUser?.email}</div>
+            <div className="profile__name">
+              {auth?.currentUser?.displayName || "유저1"}
+            </div>
           </div>
         </div>
         <div>
-          <Link to="/" className="profile__logout">
+          <Link to="/" className="profile__logout" onClick={onSignOut}>
             로그아웃
           </Link>
         </div>
